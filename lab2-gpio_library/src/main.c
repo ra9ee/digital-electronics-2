a1c8584b1269238e3a76876e8e6147fb9a06260f
ra9ee <240367@vutbr.cz>
2023-10-04 12:42:58 +0200

    edited

--- lab2-gpio_library/src/main.c
+++ lab2-gpio_library/src/main.c
@@ -1,9 +1,11 @@
 /***********************************************************************
  * 
- * Blink a LED in Arduino-style and use function from the delay library.
+ * Blink two LEDs in Arduino-, register-, and library-style. Use 
+ * function from delay library to slow down blinking.
+ * 
  * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
  *
- * Copyright (c) 2022 Tomas Fryza
+ * Copyright (c) 2019 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  * 
@@ -11,48 +13,49 @@
 
 
 /* Defines -----------------------------------------------------------*/
-#define LED_GREEN PB5   // PB5 is AVR pin where green on-board LED 
-
+#define LED_GREEN PB5   // PB5 is AVR pin where green on-board LED
                         // is connected
+#define LED_RED PB0     // Off-board LED
 #define SHORT_DELAY 250 // Delay in milliseconds
 #ifndef F_CPU
 # define F_CPU 16000000 // CPU frequency in Hz required for delay funcs
 #endif
 
-#define LED_GREEN_1 PB0   // PB5 is AVR pin where green on-board LED 
-                        // is connected
-#define SHORT_DELAY 250 // Delay in milliseconds
-#ifndef F_CPU
-# define F_CPU 16000000 // CPU frequency in Hz required for delay funcs
-#endif
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>     // AVR device-specific IO definitions
 #include <util/delay.h> // Functions for busy-wait delay loops
+#include <gpio.h>       // GPIO library for AVR-GCC
 
 
 // -----
 // This part is needed to use Arduino functions but also physical pin
 // names. We are using Arduino-style just to simplify the first lab.
 #include "Arduino.h"
-#define PB5 13          // In Arduino world, PB5 is called "13"
-#define PB0 8 
+#define PB5 5         // In Arduino world, PB5 is called "13"
+#define PB0 0
 // -----
 
 
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Toggle one LED and use delay library.
+ * Purpose:  Toggle LEDs and use delay library.
  * Returns:  none
  **********************************************************************/
 int main(void)
 {
     uint8_t led_value = 0;  // Local variable to keep LED status
 
-    // Set pin where on-board LED is connected as output
-    pinMode(LED_GREEN, OUTPUT);
-    pinMode(LED_GREEN_1, OUTPUT);
+    // Set pins where LEDs are connected as output
+    // Ver 1: Arduino style
+   // pinMode(LED_GREEN, OUTPUT);
+    // pinMode(LED_RED, OUTPUT);
+
+    // Ver 2: Low-level (register) style
+      DDRB = DDRB | (1 << LED_GREEN);
+      DDRB = DDRB | (1 << LED_RED);
+    // Ver 3: Library function style
 
     // Infinite loop
     while (1)
@@ -63,19 +66,24 @@ int main(void)
         // Change LED value
         if (led_value == 0) {
             led_value = 1;
-            // Set pin to HIGH
-            digitalWrite(LED_GREEN, HIGH);
-            digitalWrite(LED_GREEN_1, HIGH);
+            // Set pin(s) to HIGH
+           // digitalWrite(LED_GREEN, HIGH);
+            //digitalWrite(LED_RED, HIGH);
+            // PORTB = ((1 << LED_GREEN) | (1 << LED_RED));
+            GPIO_write_low(&PORTB, LED_GREEN);
+            GPIO_write_low(&PORTB, LED_RED);
         }
         else {
             led_value = 0;
-            // Clear pin to LOW
-            digitalWrite(LED_GREEN, LOW);
-            digitalWrite(LED_GREEN_1, LOW);
+            // Clear pin(s) to LOW
+            //digitalWrite(LED_GREEN, LOW);
+            //digitalWrite(LED_RED, LOW);
+            // PORTB = ((0 << LED_GREEN) | (0 << LED_RED));
+            GPIO_write_low(&PORTB, LED_GREEN);
+            GPIO_write_low(&PORTB, LED_RED);
         }
     }
 
     // Will never reach this
     return 0;
 }
-
